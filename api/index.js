0742d047b3ac0b34f71c4baaf31fd9a6b2d150f8
smarttechfire <educationfire552@gmail.com>
2024-02-26 19:09:48 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -18,4 +19,6 @@ const app = express();
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000! ')
-})
+});
+
+app.use("/api/user",userRoutes);
